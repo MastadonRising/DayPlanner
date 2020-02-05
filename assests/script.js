0609fd1f4c0b4eacb95a7d6136a2b9d592f572eb
Luis Sandoval <luissandoval1126@gmail.com>
2020-02-04 23:21:34 -0800

    time compare function working

--- assests/script.js
+++ assests/script.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 var today= moment().format('MMMM DD YYYY');
 console.log(today)
 var date = $('#date')
+var currenthour = moment().format('H');
+console.log(currenthour)
+
 date.text(today)
 
 var storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
@@ -20,11 +23,11 @@ if (storedPlans !== null) {
 for(i=9 ; i<= 17; i++) {
     var index = i -9
     // new row
-    var newrows = $('<div>').addClass('row hourslots').attr('hour', index);
+    var newrows = $('<div>').addClass('row hourslots').attr('id', i);
     // div with Hour
     var hourdiv = $('<div>').addClass('col-md-2 time');
-    var hourspn =$('<span>').attr('class','time').text(index);
-    var hrInput = $('<div>').addClass('col-md-9').append($('<input>').attr('id', index).text('placeholder'));
+    var hourspn =$('<span>').attr('class','time');
+    var hrInput = $('<div>').addClass('col-md-9').append($('<input>').attr('id', index).val(planTextArr[index]));
     var svBtn = $('<div>').addClass('col-md-1').append($('<i>').addClass('saveIcon').text('placeholder'))
     $('#timeslots').append(newrows);
     newrows.append(hourdiv.append(hourspn));
@@ -41,9 +44,17 @@ for(i=9 ; i<= 17; i++) {
       displayHour = i;
       ampm = "am";
     }
-    
+    hourspn.text(displayHour + ampm)
 
+    // set row color 
 
+   if(i < currenthour){
+     newrows.css("background-color","orange")
+   } else if(i === currenthour){
+    newrows.css("background-color","aqua")
+   } else {
+    newrows.css("background-color","red")
+   };
  
 
     
